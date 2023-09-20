0482e999dbaf8e2308111e5ff35df51680dccf6a
lucybeese <143455800+lucybeese@users.noreply.github.com>
2023-09-20 12:27:28 +0100

    Update getForestGaps.R

--- R/getForestGaps.R
+++ R/getForestGaps.R
@@ -4,7 +4,7 @@
 #'
 #' @usage getForestGaps(chm_layer, threshold=10, size=c(1,10^4))
 #'
-#' @param chm_layer ALS-derived Canopy Height Model (CHM) ([`raster::RasterLayer-class`]) object. An object of the class [`raster::RasterLayer-class`].
+#' @param chm_layer ALS-derived Canopy Height Model (CHM) ([`terra::SpatRaster-class`]) object. An object of the class [`terra::SpatRaster-class`].
 #' @param threshold Height threshold for gap detection. Default is 10 m.
 #' @param size A vector containing the minimum and maximum gap size - area (m2).
 #' Gaps with area < size\[1\] or area > size\[2\] are not considered. Default is 1 m2 and 1ha.
@@ -18,24 +18,24 @@
 #' # Loading terra and viridis libraries
 #' library(terra)
 #' library(viridis)
-#'
+#' 
 #' # ALS-derived CHM over Adolpho Ducke Forest Reserve - Brazilian tropical forest
-#' data(ALS_CHM_DUC)
-#'
+#' ALS_CHM_DUC<-rast(ALS_CHM_DUC)
+#' 
 #' # Plotting chm
 #' plot(ALS_CHM_DUC, col = viridis(10), main = "ALS CHM")
 #' grid()
-#' # =======================================================================#
+#' #' # =======================================================================#
 #' # Example 1: Forest Gap detection using a fixed canopy height thresholds
 #' # =======================================================================#
 #'
 #' # set height thresholds (e.g. 10 meters)
 #' threshold <- 10
 #' size <- c(1, 10^4) # m2
-#'
+#' 
 #' # Detecting forest gaps
 #' gaps_duc <- getForestGaps(chm_layer = ALS_CHM_DUC, threshold = threshold, size = size)
-#'
+#' 
 #' # Ploting gaps
 #' plot(gaps_duc, col = "red", add = TRUE, main = "Forest Canopy Gap", legend = FALSE)
 #'
@@ -46,15 +46,15 @@
 #' # set the height thresholds
 #' nthresholds <- c(10, 15, 20, 25)
 #' size <- c(1, 10^4) # m2
-#'
+#' 
 #' # creating an empy raster stack to store multiple gaps as RasterLayers
-#' gaps_stack <- stack()
-#'
+#' gaps_stack <- c()
+#' 
 #' # Gap detection
 #' for (i in nthresholds) {
 #'   gaps_i <- getForestGaps(chm_layer = ALS_CHM_DUC, threshold = i, size = size)
 #'   names(gaps_i) <- paste0("gaps_", i, "m")
-#'   gaps_stack <- stack(gaps_stack, gaps_i)
+#'   gaps_stack <- c(gaps_stack, gaps_i)
 #' }
 #'
 #' # plot gaps
